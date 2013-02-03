28983e67dfffedf54309a0df53d04b8ce028390e
Laurence Dougal Myers <jestarjokin@jestarjokin.net>
2013-02-03 14:33:16 +1100

    Re-start the implementation of the RLE encoder, this time using tests. This is still incomplete and incorrect.

--- robinpacker/packers/gfx.py
+++ robinpacker/packers/gfx.py
@@ -1,70 +1,66 @@
-def writeToArray(values, output, repeating_run):
-    length = len(values)
-    while length > 0:
-        rle_byte = min(length, 0x7F)
-        if repeating_run:
-            rle_byte &= 0x80
-        output.extend(values) # TODO: check if this method exists
-        length -= min(length, 0x7F)
+import array
+
+def writeDiscreteRun(output, discrete_values):
+    length = len(discrete_values)
+    slice_start = 0
+    while length:
+        min_length = min(length, 0x7F)
+        output.append(min_length)
+        output.extend(discrete_values[slice_start:slice_start + min_length])
+        length -= min_length
+        slice_start += min_length
+
+def writeRepeatingRun(output, repeating_value, repeating_length):
+    while repeating_length:
+        min_length = min(repeating_length, 0x7F)
+        output.append(min_length | 0x80)
+        output.append(repeating_value)
+        repeating_length -= min_length
 
 def encodeGfx(gfx_data):
     output = array.array('B')
     last_val = None
     repeating_run = False
+    repeating_value = None
+    repeating_length = 0
     discrete_run = False
     tmp_values = array.array('B')
     for val in gfx_data.data:
-        # If last val is different
-            # Are we already in a repeating run?
-                # end it
-                # start a new discrete run
-                # (special case for the first value?)
-            # Are we already in a discrete run?
-                # continue it
-            # Else
-                # start a new discrete run? (special case for first value?)
-        # If last val is the same
-            # Are we already in a repeating run?
-                # continue it
-            # Are we already in a discrete run?
-                # end it
-                # start a new repeating run
-            # Else
-                # start a new repeating run
-
-        if val != last_val:
-            if repeating_run:
-                writeToArray(tmp_values, output, repeating_run)
-                repeating_run = False
-                discrete_run = True
-                tmp_values = array.array('B')
-
-
-        # Use RLE flag is 0x80
-        # run length is masked with 0x7F
-
-        # End with 0xFF
-
-
-        runLength = ord(input_file.read(1))
-        if runLength == 0xFF:
-            break
-        useRLE = runLength & 0x80
-        runLength &= 0x7F
-        endpos = len(output) + runLength
-        if endpos >= max_size:
-            runLength -= endpos
-            if not runLength:
-                break
-        if useRLE:
-            val = ord(input_file.read(1))
-            for i in xrange(runLength):
-                output.append(val)
+        if last_val is None:
+            last_val = val
+            continue
+        if last_val == val:
+            if discrete_run:
+#                if len(tmp_values):
+#                    del tmp_values[-1]
+                writeDiscreteRun(output, tmp_values)
+#                tmp_values = array.array('B')
+            if not repeating_run:
+                repeating_length = 1
+            discrete_run = False
+            repeating_run = True
+            repeating_value = val
+            repeating_length += 1
         else:
-            for i in xrange(runLength):
-                val = ord(input_file.read(1))
-                output.append(val)
-    # Fill in pixels until we get to the maximum position.
-    while len(output) < max_size:
-        output.append(0x00)
+            if repeating_run:
+                writeRepeatingRun(output, repeating_value, repeating_length)
+                discrete_run = True
+                repeating_run = False
+                repeating_value = None
+                repeating_length = 0
+            else:
+                if not discrete_run:
+#                    tmp_values = array.array('B')
+                    tmp_values.append(last_val)
+                discrete_run = True
+                tmp_values.append(val)
+
+        last_val = val
+
+    if repeating_run:
+        writeRepeatingRun(output, repeating_value, repeating_length)
+    elif discrete_run:
+        writeDiscreteRun(output, tmp_values)
+
+    output.append(0xFF)
     return output
---
+++ robinpacker/test/gfx.py
@@ -0,0 +1,53 @@
+import array
+import unittest
+
+import robinpacker.packers.gfx
+
+class TestGfxRleEncoder(unittest.TestCase):
+    def setUp(self):
+        class MockGfxData(object):
+            def __init__(self):
+                self.data = None
+        self.gfx_data = MockGfxData()
+
+#    def testScenarioOne(self):
+#        self.gfx_data.data = array.array('B', [1, 1, 1, 2, 3, 4, 5, 5, 6, 0, 0, 0])
+#        expected = array.array('B', [0x83, 1, 0x05, 2, 3, 4, 5, 5, 0x01, 6, 0xFF] )
+#        result = robinpacker.packers.gfx.encodeGfx(self.gfx_data)
+#        self.assertEqual(result, expected)
+
+    def testRepeatingValues(self):
+        self.gfx_data.data = array.array('B', [1, 1, 1])
+        expected = array.array('B', [0x83, 1, 0xFF] )
+        result = robinpacker.packers.gfx.encodeGfx(self.gfx_data)
+        self.assertEqual(result, expected)
+
+    def testManyRepeatingValues(self):
+        self.gfx_data.data = array.array('B', [1] * 0x80)
+        expected = array.array('B', [0xFF, 1, 0x81, 1, 0xFF] )
+        result = robinpacker.packers.gfx.encodeGfx(self.gfx_data)
+        self.assertEqual(result, expected)
+
+    def testMultipleRepeatingValues(self):
+        self.gfx_data.data = array.array('B', [1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3])
+        expected = array.array('B', [0x85, 1, 0x84, 2, 0x83, 3, 0xFF] )
+        result = robinpacker.packers.gfx.encodeGfx(self.gfx_data)
+        self.assertEqual(result, expected)
+
+    def testDiscreteValues(self):
+        self.gfx_data.data = array.array('B', [1, 2, 3])
+        expected = array.array('B', [0x03, 1, 2, 3, 0xFF] )
+        result = robinpacker.packers.gfx.encodeGfx(self.gfx_data)
+        self.assertEqual(result, expected)
+
+    def testManyDiscreteValues(self):
+        self.gfx_data.data = array.array('B', [1, 2, 3] * 43)
+        expected = array.array('B', [0x7F] + [1, 2, 3] * 42 + [1, 0x02, 2, 3, 0xFF] )
+        result = robinpacker.packers.gfx.encodeGfx(self.gfx_data)
+        self.assertEqual(result, expected)
+
+    def testMultipleDiscreteValuesWithRepeatingValuesInTheMiddle(self):
+        self.gfx_data.data = array.array('B', [1, 2, 3, 4, 4, 4, 5, 6, 7])
+        expected = array.array('B', [0x03, 1, 2, 3, 0x83, 4, 0x03, 5, 6, 7, 0xFF] )
+        result = robinpacker.packers.gfx.encodeGfx(self.gfx_data)
+        self.assertEqual(result, expected)
